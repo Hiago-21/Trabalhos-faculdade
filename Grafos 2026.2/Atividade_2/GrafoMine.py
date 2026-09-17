29d19943c84294d51e8c6a1ebeca9f5484a7d9fc
Hiago Lucas <hiago.lucas.trabalho@gmail.com>
2026-09-17 17:06:16 -0300

    Feito o grafo G, Ainda falta o complementar

--- Grafos 2026.2/Atividade_2/GrafoMine.py
+++ Grafos 2026.2/Atividade_2/GrafoMine.py
@@ -3,17 +3,43 @@ import matplotlib.pyplot as plt
 
 G = nx.Graph()
 
-G.add_node("w")
-G.add_node("x")
-G.add_node("y")
-G.add_node("z")
+G.add_node("Graveto"     )
+G.add_node("Açucar"      )
+G.add_node("Madeira"     )
+G.add_node("Vidro"       )
+G.add_node("Teia"        )
+G.add_node("Ferro"       )
+G.add_node("Redstone"    )
+G.add_node("Corante"     )
+G.add_node("OlhoDeAranha")
+G.add_node("Cogumelo"    )
 
-G.add_edge("w", "x")
-G.add_edge("z", "w")
-G.add_edge("w", "y")
-G.add_edge("y", "z")
-G.add_edge("x", "z")
+G.add_edge("Vidro", "Vidro"  )
+G.add_edge("Vidro", "Corante")
 
-G.add_edges_from([("x", "y"),("y", "x"), ("y", "z")])
-nx.draw(G, with_labels = True)
+G.add_edge("Ferro", "Ferro"   )
+
+G.add_edge("Açucar", "Cogumelo")
+
+G.add_edge("Graveto", "Graveto" )
+G.add_edge("Graveto", "Madeira" )
+G.add_edge("Graveto", "Ferro"   )
+G.add_edge("Graveto", "Redstone")
+G.add_edge("Graveto", "Teia"    )
+
+G.add_edge("Madeira", "Madeira" )
+G.add_edge("Madeira", "Redstone")
+G.add_edge("Madeira", "Ferro"   )
+G.add_edge("Madeira", "Teia"    )
+
+G.add_edge("Redstone", "Redstone")
+G.add_edge("Redstone", "Ferro"   )
+
+G.add_edge("OlhoDeAranha", "Açucar"  )
+G.add_edge("OlhoDeAranha", "Cogumelo")
+
+plt.figure(figsize=(10, 8))
+posicao = nx.circular_layout(G)
+nx.draw(G, pos=posicao, with_labels=True, node_color='lightgreen', node_size=2500, font_weight='bold', font_size=9)
+
 plt.show()
